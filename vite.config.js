c74e67962d0ad94c323054a9c286b045932dfa6b
gnawtough <avkoller01@hotmail.com>
2024-03-19 22:30:22 -0500

    add base public path

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/portfolio/",
+
   plugins: [
     vue(),
   ],
@@ -15,3 +15,4 @@ export default defineConfig({
     }
   }
 })
+
