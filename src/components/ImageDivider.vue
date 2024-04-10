8df8b8a6a6cc60d5d06f77a41af5853d8631177a
gnawtough <avkoller01@hotmail.com>
2024-04-10 18:21:57 -0500

    add imageDivider component

---
+++ src/components/ImageDivider.vue
@@ -0,0 +1,30 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="image-divider">
+    <img src="@/assets/divider-image.svg" alt="Divider" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ImageDivider',
+  // props or data go here
+}
+</script>
+
+<style scoped>
+.image-divider {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  margin: 20px 0;
+}
+
+.image-divider img {
+  max-width: 100%;
+  height: auto;
+}
+</style>
