8741d54383063403cc08874e60237e38b0e39610
gnawtough <avkoller01@hotmail.com>
2024-04-10 21:37:10 -0500

    fixed image paths

--- src/data/projectData.js
+++ src/data/projectData.js
@@ -3,15 +3,15 @@ export const categories = [
         id: 1,
         name: 'Web Development',
         projects: [
-            { id: 1, title: 'Project 1', imageUrl: './public/projectImages/colorfulRat.jpg', description: 'Description of Project 1' },
-            { id: 2, title: 'Portfolio', imageUrl: './public/projectImages/colorfulRat.jpg', description: 'This is how I created my portfolio.'}
+            { id: 1, title: 'Project 1', imageUrl: './projectImages/colorfulRat.jpg', description: 'Description of Project 1' },
+            { id: 2, title: 'Portfolio', imageUrl: './projectImages/colorfulRat.jpg', description: 'This is how I created my portfolio.'}
         ]
     },
     {
         id: 2,
         name: 'Product Design',
         projects: [
-            { id: 1, title: 'Small Lamp', imageUrl: './public/projectImages/colorfulRat.jpg', description: "This is a small lamp I made."}
+            { id: 1, title: 'Small Lamp', imageUrl: './projectImages/colorfulRat.jpg', description: "This is a small lamp I made."}
         ]
     }
     // More categories...
