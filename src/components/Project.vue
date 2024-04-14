01ccb0c0ec31f9ce98ceaa09c060f5a03f45bc8e
gnawtough <avkoller01@hotmail.com>
2024-04-14 11:56:58 -0500

    add soft corners and padding to Project module

--- src/components/Project.vue
+++ src/components/Project.vue
@@ -8,19 +8,27 @@ const props = defineProps({
 <template>
   <div class="project-tile">
     <img :src="props.project.imageUrl" class="project-image" alt="Bingus">
-    <h3>{{ props.project.title }}</h3>
-    <p>{{ props.project.description }}</p>
+    <h3 class="project-text">{{ props.project.title }}</h3>
+    <p class="project-text">{{ props.project.description }}</p>
   </div>
 </template>
 
 <style scoped>
 .project-tile {
+  background: #282828;
   margin-right: 20px; /* Space between tiles */
   width: 200px;
+  border-radius: 20px;
 }
 
 .project-image {
-  width: 100%;
+  width: 90%;
   height: auto; /* Maintain aspect ratio */
+  margin: 20px 20px 20px 10px;
+  border-radius: 20px;
+}
+
+.project-text {
+  margin: 0 0 0 10px;
 }
 </style>
